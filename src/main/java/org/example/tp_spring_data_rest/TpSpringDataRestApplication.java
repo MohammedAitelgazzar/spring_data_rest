f54ffd2fbeb60a9c11077eb39f700d3a33e3760b
Aitelgazzar <mohamedaitelgazzar@gmail.com>
2024-11-09 12:43:54 +0100

    v1

---
+++ src/main/java/org/example/tp_spring_data_rest/TpSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package org.example.tp_spring_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpSpringDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tp_spring_data_rest/TpSpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tp_spring_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
